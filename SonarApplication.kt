079225497d941bbc97081ffaef4fcaa79b651091
Intelcold <>
2025-09-14 19:34:18 +0000

    Fix: Correct multidex implementation and fix build errors

---
+++ SonarApplication.kt
@@ -0,0 +1,9 @@
+package com.example.fogsonar
+
+import androidx.multidex.MultiDexApplication
+
+class SonarApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
